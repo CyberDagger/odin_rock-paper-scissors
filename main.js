1519a05166e106e90e9375cabe2998f89136feb2
CyberDagger <cyberdagger003@gmail.com>
2025-03-28 17:44:59 +0000

    Implement game result logic

--- main.js
+++ main.js
@@ -17,3 +17,54 @@ function getHumanChoice() {
     let hPlay = prompt("Rock, Paper, or Scissors?");
     return hPlay;
 }
+
+function playRound(humanChoice, computerChoice) {
+    let result;
+    humanChoice = humanChoice.toLowerCase();
+    switch(humanChoice) {
+        case "rock":
+            switch(computerChoice) {
+                case "rock":
+                    result = "tie";
+                    break;
+                case "paper":
+                    result = "loss";
+                    break;
+                case "scissors":
+                    result = "win";
+                    break;
+            }
+            break;
+        case "paper":
+            switch(computerChoice) {
+                case "rock":
+                    result = "win";
+                    break;
+                case "paper":
+                    result = "tie";
+                    break;
+                case "scissors":
+                    result = "loss";
+                    break;
+            }
+            break;
+        case "scissors":
+            switch(computerChoice) {
+                case "rock":
+                    result = "loss";
+                    break;
+                case "paper":
+                    result = "win";
+                    break;
+                case "scissors":
+                    result = "tie";
+                    break;
+            }
+            break;
+    }
+    console.log(result);
+}
+
+const cSelect = getComputerChoice();
+const hSelect = getHumanChoice();
+playRound(hSelect, cSelect);
