3603b95579a8ace77d923e1490cf8fc6ec75abd9
CyberDagger <cyberdagger003@gmail.com>
2025-03-28 17:23:03 +0000

    Implement computer choice

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+function getComputerChoice() {
+    let play = Math.floor(Math.random() * 3);
+    switch(play) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
