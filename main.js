0fb05688b875945d8d06f372bcacf19fa6984665
CyberDagger <cyberdagger003@gmail.com>
2025-03-31 10:13:32 +0100

    5 rounds game
    playRound wrapped in playGame function that calls it 5 times. Score tallied at the end.

--- main.js
+++ main.js
@@ -18,53 +18,84 @@ function getHumanChoice() {
     return hPlay;
 }
 
-function playRound(humanChoice, computerChoice) {
-    let result;
-    humanChoice = humanChoice.toLowerCase();
-    switch(humanChoice) {
-        case "rock":
-            switch(computerChoice) {
-                case "rock":
-                    result = "A tie! Both chose Rock.";
-                    break;
-                case "paper":
-                    result = "You lose! Paper beats Rock.";
-                    break;
-                case "scissors":
-                    result = "You win! Rock beats Scissors.";
-                    break;
-            }
-            break;
-        case "paper":
-            switch(computerChoice) {
-                case "rock":
-                    result = "You win! Paper beats Rock.";
-                    break;
-                case "paper":
-                    result = "A tie! Both chose Paper.";
-                    break;
-                case "scissors":
-                    result = "You lose! Scissors beats Paper.";
-                    break;
-            }
-            break;
-        case "scissors":
-            switch(computerChoice) {
-                case "rock":
-                    result = "You lose! Rock beats Scissors.";
-                    break;
-                case "paper":
-                    result = "You win! Scissors beats Paper.";
-                    break;
-                case "scissors":
-                    result = "A tie! Both chose Scissors.";
-                    break;
-            }
-            break;
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        let result;
+        humanChoice = humanChoice.toLowerCase();
+        switch(humanChoice) {
+            case "rock":
+                switch(computerChoice) {
+                    case "rock":
+                        console.log("A tie! Both chose Rock.");
+                        result = "tie";
+                        break;
+                    case "paper":
+                        console.log("You lose! Paper beats Rock.");
+                        result = "loss";
+                        break;
+                    case "scissors":
+                        console.log("You win! Rock beats Scissors.");
+                        result = "win";
+                        break;
+                }
+                break;
+            case "paper":
+                switch(computerChoice) {
+                    case "rock":
+                        console.log("You win! Paper beats Rock.");
+                        result = "win";
+                        break;
+                    case "paper":
+                        console.log("A tie! Both chose Paper.");
+                        result = "tie";
+                        break;
+                    case "scissors":
+                        console.log("You lose! Scissors beats Paper.");
+                        result = "loss";
+                        break;
+                }
+                break;
+            case "scissors":
+                switch(computerChoice) {
+                    case "rock":
+                        console.log("You lose! Rock beats Scissors.");
+                        result = "loss";
+                        break;
+                    case "paper":
+                        console.log("You win! Scissors beats Paper.");
+                        result = "win";
+                        break;
+                    case "scissors":
+                        console.log("A tie! Both chose Scissors.");
+                        result = "tie";
+                        break;
+                }
+                break;
+        }
+        return result;
     }
-    console.log(result);
+    for (let i = 0; i < 5; i++) {
+        let result;
+        let computerChoice = getComputerChoice();
+        let humanChoice = getHumanChoice();
+        result = playRound(humanChoice, computerChoice);
+        switch(result) {
+            case "win":
+                humanScore++;
+                break;
+            case "loss": 
+                computerScore++;
+                break;
+        }
+    }
+    if (humanScore > computerScore) {
+        console.log("You win!");
+    } else {
+        console.log("You lose!");
+    }
 }
 
-const cSelect = getComputerChoice();
-const hSelect = getHumanChoice();
-playRound(hSelect, cSelect);
+playGame();
